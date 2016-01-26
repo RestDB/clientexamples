21d553a90976391faa0ef1214f002f643a409732
restdbjones <jones@restdb.io>
2016-01-26 18:39:48 +0100

    CRUD example

--- Python/sample.py
+++ Python/sample.py
@@ -1,11 +1,30 @@
 import requests
-headers = {'x-apikey': '58aef41e2c4ed27f0da9dd05a1720d6698be3'}
+import json
+# URL to restdb.io database
+dburl = 'https://rdb-examples.restdb.io/rest/stuff'
+
+# public api key
+headers = {'x-apikey': '58aef41e2c4ed27f0da9dd05a1720d6698be3', 'Content-Type': 'application/json'}
 params = {'sort': 'title'}
-resp = requests.get('https://rdb-examples.restdb.io/rest/stuff', params=params, headers=headers)
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /stuff/ {}'.format(resp.status_code))
+payload = {"title":"Python title", "description": "POST from Pyhon code"}
 
+# POST data
+r = requests.post(dburl, headers = headers, data = json.dumps(payload))
 
-for stuff in resp.json():
+print(r.status_code, r.text)
+
+# GET data
+r = requests.get(dburl, params=params, headers=headers)
+
+print(r.status_code, r.text)
+
+# print records
+for stuff in r.json():
     print('{} {} {}'.format(stuff['_id'], stuff['title'], stuff['description']))
+
+# PUT an update to record 56992036c429a20e00000621
+requests.put(dburl+"/56992036c429a20e00000621", headers = headers, data = json.dumps({"title":"changed by Python PUT"}))
+print(r.status_code, r.text)
+
+# DELETE example code
+#requests.delete(dburl+"/56992036c429a20e00000621")
