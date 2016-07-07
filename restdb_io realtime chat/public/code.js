e1e6f4826d34425eacdf12805a8b39e83f64cd56
restdbjones <jones@restdb.io>
2016-07-07 16:57:07 +0200

    Included PUT and DELETE

--- restdb_io realtime chat/public/code.js
+++ restdb_io realtime chat/public/code.js
@@ -18,41 +18,68 @@ $( document ).ready(function() {
     }
     
     $("#oneliner").keyup(function(e){
-    	if(e.keyCode == 13 && $(this).val() !== '')
-    	{
-    	    hightlight("#input_block");
-    		var payload = {"oneliner": $(this).val()};
-    		$(this).val("");
-    		var newline = new db.chat(payload);
-    		newline.save();
-    	}
+        if(e.keyCode == 13 && $(this).val() !== '')
+        {
+            hightlight("#input_block");
+            var payload = {"oneliner": $(this).val()};
+            $(this).val("");
+            var newline = new db.chat(payload);
+            newline.save();
+        }
     });
     
     db.chat.on('POST', function(err, mess) {
-    	hightlight("#post_block");
-    	var line = $("<div>").text(mess.data.oneliner);
-    	line.hide();
-    	$("#messages").prepend(line);
-    	line.toggle("scale")
+        hightlight("#post_block");
+        var text = $("<div>").text(mess.data.oneliner);
+        text.attr("id", mess.data._id);
+        text.hide();
+        $("#messages").prepend(text);
+        text.toggle("scale");
+        text.dblclick(function() {
+          var deleteme = new db.chat({_id: mess.data._id});
+          deleteme.delete();
+        });
     });
+    
+    db.chat.on('PUT', function(err, mess) {
+        hightlight("#put_block");
+        db.chat.find({"_id": mess.data}, {}, function(err, updatedline){
+            $("#"+mess.data).text(updatedline[0].oneliner);
+        });
+    });
+    
+    // double click on text to delete
+    db.chat.on('DELETE', function(err, mess) {
+        hightlight("#delete_block");
+        _.each(mess.data, function(oneid){
+            $("#"+oneid).remove();    
+        });
+    });
+    
     db.on("CONNECT", function() {
-    	hightlight("#connect_block");
-    	$("#status").addClass("online").removeClass("offline").text("online");
-    	$("#messages").empty();
-    	db.chat.find({}, {"$max": 100,"$orderby": {"_created": -1}}, function(err, lines){
-    	    _.each(lines, function(aline){
-    	        $("#messages").append($("<div>").text(aline.oneliner));        
-    	    });
-    	})
-    	
+        hightlight("#connect_block");
+        $("#status").addClass("online").removeClass("offline").text("online");
+        $("#messages").empty();
+        db.chat.find({}, {"$max": 100,"$orderby": {"_created": -1}}, function(err, lines){
+            _.each(lines, function(aline){
+                var text = $("<div>").text(aline.oneliner);
+                text.attr("id", aline._id);
+                $("#messages").append(text);
+                text.dblclick(function() {
+                  var deleteme = new db.chat({_id: aline._id});
+                  deleteme.delete();
+                });
+            });
+        })
+        
     });
     db.on("DISCONNECT", function() {
-    	hightlight("#disconnect_block");
-    	$("#status").addClass("online").addClass("offline").text("off");
+        hightlight("#disconnect_block");
+        $("#status").addClass("online").addClass("offline").text("off");
     });
     db.on("RECONNECT", function() {
-    	hightlight("#reconnect_block");
-    	$("#status").addClass("online").removeClass("offline").text("Back again");
+        hightlight("#reconnect_block");
+        $("#status").addClass("online").removeClass("offline").text("Back again");
     });
-   
+    
 });
