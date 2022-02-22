cebbd547fbd59e4854ad858afd50824f348da13c
Jon Erik Solheim <jones@restdb.io>
2022-02-22 19:04:24 +0100

    Update upload.py

--- Python File Upload/upload.py
+++ Python File Upload/upload.py
@@ -1,7 +1,12 @@
 import requests
-latest_file = './image.jpeg'
-headers = {'x-apikey': '{MY_API_KEY_HERE}'}
 url = "https://{MYDATABASE_NAME_HERE}.restdb.io/media"
-files = {'file': open(latest_file, 'rb')}
-r = requests.post(url, files=files, headers=headers)
-print(r.status_code, r.text)
+payload={}
+files=[
+  ('file',('image.jpeg',open('./image.jpeg','rb'),'image/jpeg'))
+]
+headers = {
+  'x-apikey': '{MY_API_KEY_HERE}'
+}
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print(response.text)
